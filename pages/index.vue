3d75b94c8bb7df2d3d117a5ba56df30825a879d4
Brian McMillen <brianmcmillen1@gmail.com>
2021-01-24 17:48:20 -0500

    init:

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">capo</h1>
+      <nuxt-content v-bind="{ document }"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  asyncData: async ({ $content }) => {
+    const document = await $content('hello').fetch();
+    return { document }
+  }
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.nuxt-content {
+  max-width: 400px;
+}
+</style>
